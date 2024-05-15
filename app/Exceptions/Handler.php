38678ff5f34e790ddb803ae452c6fb656c0911f5
chillingloccini <chillingloccini1@gmail.com>
2024-05-15 15:34:41 +0100

    Setting up error handling in the application.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,14 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -21,10 +27,35 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function render($request, Throwable $e)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        if ($e instanceof ModelNotFoundException || $e instanceof NotFoundHttpException) {
+            $error = 'Route not found';
+            if ($e instanceof ModelNotFoundException) {
+                $modelName = class_basename($e->getModel());
+                $error = "$modelName not found";
+            }
+            return errorResponse($error, 404);
+        }
+        elseif ($e instanceof ValidationException) {
+            return errorResponse($e->validator->errors()->first(), Response::HTTP_UNPROCESSABLE_ENTITY, $e->validator->errors());
+        }
+
+        elseif ($e instanceof ClientErrorException) {
+            return errorResponse($e->getMessage(),$e->getCode());
+        }
+
+        elseif ($e instanceof ThrottleRequestsException) {
+            return errorResponse('Max attempts exceeded.Retry later.',$e->getCode());
+        }
+
+        Log::error($e->getMessage());
+
+        return  errorResponse($e->getMessage(),Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
