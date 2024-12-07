98306220c0ab43d229e28fa6078f351028e1ee16
chillingloccini <chillingloccini1@gmail.com>
2024-12-07 03:09:00 +0100

    ♻️ refactor(frontend): modularize components for better reusability

--- database/migrations/2024_05_18_094037_create_transactions_table.php
+++ database/migrations/2024_05_18_094037_create_transactions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->uuid('ueui');
+            $table->uuid('udeui');
             $table->string('reference')->unique();
             $table->string('type');
             $table->string('description');
